feb1e7340b958fe6ea51c478f23ebfcf4274f632
Steven Excelmans <steven.excelmans@telenetgroup.be>
2023-03-09 14:39:02 +0100

    [steven-excelmans] init repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div class="h-screen w-screen bg-slate-700">
+            <div class="container mx-auto pt-12">
+                <header className="select-none text-4xl font-thin  text-teal-50">
+                    Work in progress <span class="animate-pulse">...</span>
+                </header>
+            </div>
+        </div>
+    );
+}
+
+export default App;
